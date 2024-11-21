a514f8f2acb34880633eee6e289e9622683025f1
kirnic <marcelokirnic@gmail.com>
2024-11-21 17:46:08 -0300

    redo .env variables

--- db/pool.js
+++ db/pool.js
@@ -3,9 +3,9 @@ require('dotenv').config(); // Load environment variables
 const { Pool } = require("pg");
 
 module.exports = new Pool({
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: process.env.PG_NAME
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE
 });
